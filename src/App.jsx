86e5470702a05cbb7b392b3482e829ab0036c08d
yusufshhzd <yusufshhzd@gmail.com>
2025-03-13 14:39:06 -0400

    headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-import './App.css'
-import {ContainedButton} from './components/ContainedButton'
-import {OutlinedButton} from './components/OutlinedButton'
-import React from 'react'
-import { Stack } from '@mui/material'
-
+import { Stack, Typography } from '@mui/material';
+import ContainedButton from './components/ContainedButton';
+import { useNavigate } from 'react-router-dom';
+import SimplePresent from './pages/SimplePresent';
 
 function App() {
-
   return (
-
-    <div className='App'>
+    <div className='App' style={{ position: 'relative', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Typography variant="h6" style={{ position: 'absolute', top: '10%', textAlign: 'center' }}>
+        Present Tenses
+      </Typography>
+      <Typography variant="h6" style={{ position: 'absolute', top: '40%', textAlign: 'center' }}>
+        Past Tenses
+      </Typography>
+      <Typography variant="h6" style={{ position: 'absolute', top: '65%', textAlign: 'center' }}>
+        Future Tenses
+      </Typography>
 
       <div>
-       <Stack spacing = {20} direction = 'row' className='button'>
-          <Stack spacing = {20} direction = 'column'>
-            <ContainedButton text = {"Simple\nPresent"} />
-            <ContainedButton text = {"Simple\nPast"} />
-            <ContainedButton text = {"Simple\nFuture"} />
+        <Stack spacing={20} direction='row' className='button'>
+          <Stack spacing={20} direction='column'>
+            <ContainedButton text={"Simple\nPresent"} />
+            <ContainedButton text={"Simple\nPast"} />
+            <ContainedButton text={"Simple\nFuture"} />
           </Stack>
 
-          <Stack spacing = {20} direction = 'column'>
-            <ContainedButton text = {"Present\nContinuous"} />
-            <ContainedButton text = {"Past\nContinuous"} />
-            <ContainedButton text = {"Future\nContinuous"} />
+          <Stack spacing={20} direction='column'>
+            <ContainedButton text={"Present\nContinuous"} />
+            <ContainedButton text={"Past\nContinuous"} />
+            <ContainedButton text={"Future\nContinuous"} />
           </Stack>
 
-         <Stack spacing = {20} direction = 'column'>
-            <ContainedButton text = {"Present\nPerfect"} />
-            <ContainedButton text = {"Past\nPerfect"} />
-            <ContainedButton text = {"Future\nPerfect"} />
+          <Stack spacing={20} direction='column'>
+            <ContainedButton text={"Present\nPerfect"} />
+            <ContainedButton text={"Past\nPerfect"} />
+            <ContainedButton text={"Future\nPerfect"} />
           </Stack>
 
-          <Stack spacing = {20} direction = 'column'>
-            <ContainedButton text = {"Present\nPerfect\nContinuous"} />
-            <ContainedButton text = {"Past\nPerfect\nContinuous"} />
-            <ContainedButton text = {"Future\nPerfect\nContinuous"} />
+          <Stack spacing={20} direction='column'>
+            <ContainedButton text={"Present\nPerfect\nContinuous"} />
+            <ContainedButton text={"Past\nPerfect\nContinuous"} />
+            <ContainedButton text={"Future\nPerfect\nContinuous"} />
           </Stack>
         </Stack>
       </div>
-
     </div>
-
-  )
-
+  );
 }
 
-export default App
+export default App;
